d6cb233380f9159f1de2e24909f104526f5cb19e
Krish <krishnerkar@gmail.com>
2023-01-03 17:09:08 +0530

    load fonts

--- next.config.js
+++ next.config.js
@@ -2,7 +2,17 @@
 const nextConfig = {
   experimental: {
     appDir: true,
+    fontLoaders: [
+      {
+        loader: "@next/font/google",
+        options: {
+          subsets: ["latin"],
+          weight: ["400", "500", "600", "700", "800"],
+          display: "swap",
+        },
+      },
+    ],
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
