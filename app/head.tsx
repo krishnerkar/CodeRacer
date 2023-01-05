cf9dbdb5fc911ef413acff3d28618373e892475b
Krish <krishnerkar@gmail.com>
2023-01-05 10:16:43 +0530

    update meta tags

--- app/head.tsx
+++ app/head.tsx
@@ -7,13 +7,16 @@ export default function Head() {
       <meta name="keywords" content="typing game, coders, typing test" />
       <meta name="robots" content="index, follow" />
       <meta name="language" content="English" />
-      <meta property="og:image" content="/meta.png" />
-      <meta name="twitter:image" content="/meta.png" />
       <link rel="icon" href="/favicon.ico" />
-      <meta property="twitter:card" content="/meta.png" />
+
+      <meta property="og:image" content="/meta.png" />
+      <meta property="og:description" content="A Typing game for coders" />
+
+      <meta name="twitter:image" content="/meta.png" />
+      <meta name="twitter:card" content="summary_large_image" />
       <meta property="twitter:url" content="https://www.coderacer.tech/" />
       <meta property="twitter:title" content="CodeRacer" />
-      <meta property="twitter:description" content=" Typing game for coders" />
+      <meta property="twitter:description" content="A Typing game for coders" />
       <meta property="twitter:image" content="/meta.png"></meta>
     </>
   );
