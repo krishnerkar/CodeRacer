83da73a647500f0648fde556302f52a0be50a936
Krish <krishnerkar@gmail.com>
2023-01-05 13:23:12 +0530

    ux

--- components/CodeInput/CodeInput.tsx
+++ components/CodeInput/CodeInput.tsx
@@ -16,13 +16,14 @@ export default function CodeInput({
 }) {
   return (
     <input
+      autoFocus
       id="input"
       autoComplete="off"
       className={`${styles.input} ${source_code.className}`}
       value={currWord}
       onChange={onChange}
       onKeyDown={handleKeyDown}
-      placeholder={isRaceFinished ? "Type the above code here..." : ""}
+      placeholder={!isRaceFinished ? "Type the above code here..." : ""}
       disabled={isRaceFinished}
       style={{
         border: error ? "2px solid #ff3333" : "1px solid #fff",
